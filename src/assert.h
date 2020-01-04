a92daa6b3014671c83c37ba4e102ad377d63418f
Nufflee <zifcani@gmail.com>
2020-01-04 02:05:53 +0100

    Add assert.h.
    This is a custom assert implementation, not a standard C "assert.h" one.

---
+++ src/assert.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#ifndef RELEASE
+#include <stdbool.h>
+#include "serial_port.h"
+
+#define ASSERT_M(condition, message)                                                         \
+  do                                                                                         \
+  {                                                                                          \
+    if (!(condition))                                                                        \
+    {                                                                                        \
+      serial_port_printf(COM1, "[%s:%d] %s: %s", __FILE__, __LINE__, __FUNCTION__, message); \
+    }                                                                                        \
+    while (true)                                                                             \
+    {                                                                                        \
+    }                                                                                        \
+  } while (false)
+
+#define ASSERT(condition) ASSERT_M(condition, "Assert '" #condition "' failed!\n")
+#define ASSERT_NOT_REACHED() ASSERT_M(false, "Assert shouldn't have been reached!\n")
+#else
+#define ASSERT_M(condition, message)
+#define ASSERT(condition)
+#define ASSERT_NOT_REACHED()
+#endif
