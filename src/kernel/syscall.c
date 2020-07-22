186bd6f3cf3a206da1dccac6178d98c44d53fd45
Nufflee <zifcani@gmail.com>
2020-07-22 12:28:57 +0200

    Refactor some syscalls.
    * Clean upm interrupts.h and use ';' instead of newlines
    * Create sys$ioctl with more checks for unimplemented requests

--- src/kernel/syscall.c
+++ src/kernel/syscall.c
@@ -7,17 +7,19 @@
 
 //#define SYSCALL_DEBUG
 
-volatile u32* syscall_return = 0;
+volatile u32 *syscall_return = 0;
 
-struct iovec {
-    void  *iov_base;    /* Starting address */
-    size_t iov_len;     /* Number of bytes to transfer */
-};
+typedef struct iovec
+{
+  void *base;    // Starting address
+  size_t length; // Number of bytes to transfer
+} iovec;
 
 void syscall_interrupt_handler(register_info *);
 void sys$exit(i32);
-int sys$write(u32, char *, size_t);
-int sys$writev(int, const struct iovec *, int);
+int sys$write(int, char *, size_t);
+int sys$writev(int, const iovec *, int);
+int sys$ioctl(int, u32, void *);
 
 void syscall_init()
 {
@@ -37,19 +39,16 @@ void syscall_interrupt_handler(register_info *info)
     *syscall_return = sys$write(info->ebx, (char *)info->ecx, info->edx);
     break;
   case SYSCALL_IOCTL:
-    // TODO: sys$ioctl not implemented. Fake success for now.
-    *syscall_return = 0;
+    *syscall_return = sys$ioctl(info->ebx, info->ecx, (void *)info->edx);
     break;
   case SYSCALL_MODIFY_LDT:
     // TODO: sys$set_modify_ldt is not implemnted.
-    // TODO: This syscall is supposed to return something.
     break;
   case SYSCALL_WRITEV:
-    *syscall_return = sys$writev(info->ebx, (const struct iovec*)info->ecx, info->edx);
+    *syscall_return = sys$writev(info->ebx, (const iovec *)info->ecx, info->edx);
     break;
   case SYSCALL_SET_THREAD_AREA:
     // TODO: sys$set_thread_area is not implemnted.
-    // TODO: This syscall is supposed to return something.
     break;
   case SYSCALL_EXIT_GROUP:
     // TODO: This should exit all threads but we have no concept of threads right now.
@@ -57,7 +56,6 @@ void syscall_interrupt_handler(register_info *info)
     break;
   case SYSCALL_SET_TID_ADDRESS:
     // TODO: sys$set_tid_address is not implemnted.
-    // TODO: This syscall is supposed to return something.
     break;
   default:
     // TODO: ASSERT_ALWAYS formatting
@@ -80,9 +78,13 @@ void sys$exit(i32 return_code)
   asm volatile("jmp process_kernel_return");
 }
 
-int sys$write(u32 fd, char *buffer, size_t length)
+#define FD_STDOUT 1
+
+int sys$write(int fd, char *buffer, size_t length)
 {
-  if (fd == 1)
+  ASSERT(buffer);
+
+  if (fd == FD_STDOUT)
   {
     for (size_t i = 0; i < length; i++)
     {
@@ -90,17 +92,41 @@ int sys$write(u32 fd, char *buffer, size_t length)
     }
     return length;
   }
+
   serial_port_printf(COM1, "fd %d not implemented\n", fd);
   ASSERT_ALWAYS("");
 }
 
-int sys$writev(int fd, const struct iovec *iov, int iovcnt) {
+#define TIOCGWINSZ 0x5413
+
+int sys$ioctl(int fd, u32 request, void *arg)
+{
+  if (fd == 1 && request == TIOCGWINSZ)
+  {
+    return 0;
+  }
+
+  serial_port_printf(COM1, "Unimplemented ioctl request: %d\n", request);
+  ASSERT_ALWAYS("");
+}
+
+int sys$writev(int fd, const iovec *iov, int iovcnt)
+{
+  ASSERT(iov);
+
   int total_bytes_written = 0;
-  for(int i = 0; i < iovcnt; ++i) {
-    int bytes_written = sys$write(fd, iov[i].iov_base, iov[i].iov_len);
-    if(bytes_written < 0)
+
+  for (int i = 0; i < iovcnt; ++i)
+  {
+    int bytes_written = sys$write(fd, iov[i].base, iov[i].length);
+
+    if (bytes_written < 0)
+    {
       return bytes_written;
+    }
+
     total_bytes_written += bytes_written;
   }
+
   return total_bytes_written;
 }
