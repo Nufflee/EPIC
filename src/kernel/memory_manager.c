d5f6b7dfdfb403e0f16a03d69e023891c9ba61e7
Nufflee <zifcani@gmail.com>
2020-07-24 15:53:09 +0200

    Make page allocator actually page-aligned.
    Previously, the page allocator wouldn't return page-aligned addresses because the base address was based off of the length of the page bitmap which invalidates its alignment.
    We now account for that and return correctly aligned addresses.

--- src/kernel/memory_manager.c
+++ src/kernel/memory_manager.c
@@ -1,5 +1,6 @@
 #include <libk/assert.h>
 #include <libk/bits.h>
+#include <libk/math.h>
 #include "memory_manager.h"
 
 extern u32 KERNEL_END;
@@ -7,9 +8,11 @@ extern u32 KERNEL_END;
 static u8 *page_bitmap;
 static u32 page_bitmap_length;
 
-static u32 base_address = 0x400000; // ((u32)&KERNEL_END + 0x100000);
+static u32 bitmap_base_address = (u32)&KERNEL_END + 0x100;
 static u32 memory_end;
 static u32 memory_page_count;
+
+static u32 allocator_base_address;
 
 void memory_manager_init(multiboot_info_t *multiboot)
 {
@@ -29,19 +32,21 @@ void memory_manager_init(multiboot_info_t *multiboot)
 
   ASSERT(memory_end > 0 && "No available memory regions found!");
 
-  u32 available_memory = memory_end - base_address;
+  u32 available_memory = memory_end - bitmap_base_address;
   memory_page_count = available_memory / PAGE_SIZE;
   page_bitmap_length = memory_page_count / 8;
 
-  memset((u32 *)base_address, 0, page_bitmap_length);
-  page_bitmap = (u8 *)base_address;
+  memset((u32 *)bitmap_base_address, 0, page_bitmap_length);
+  page_bitmap = (u8 *)bitmap_base_address;
+
+  allocator_base_address = divide_and_round_up(bitmap_base_address + page_bitmap_length, PAGE_SIZE) * PAGE_SIZE;
 
   serial_port_printf(COM1, "System has %d bytes of memory avilable\n", available_memory);
 }
 
 u32 memory_manager_get_available_memory()
 {
-  return memory_end - (base_address + page_bitmap_length);
+  return memory_end - allocator_base_address;
 }
 
 u32 allocate_physical_page()
@@ -56,7 +61,7 @@ u32 allocate_physical_page()
       {
         BIT_SET(page_bitmap[i], j);
 
-        u32 address = (page_bitmap_length + base_address) + ((i * 8) + j) * PAGE_SIZE;
+        u32 address = allocator_base_address + ((i * 8) + j) * PAGE_SIZE;
 
         return address;
       }
@@ -70,7 +75,7 @@ void free_physical_page(u32 address)
 {
   ASSERT(address <= memory_end);
 
-  u32 page_number = (address - base_address) / PAGE_SIZE;
+  u32 page_number = (address - allocator_base_address) / PAGE_SIZE;
   u32 byte_index = page_number / 8;
   u8 bit_index = page_number % 8;
   bool is_page_allocated = BIT_GET(page_bitmap[byte_index], bit_index);
